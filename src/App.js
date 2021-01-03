ca0df44ae5518dd7ea8a6fd77290f08aa0b4e08d
Noah Glaser <glaserpower@gmail.com>
2021-01-02 16:05:41 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Svelte Component in React App</h1>
+      <count-btn count="3"></count-btn>
+    </div>
+  );
+}
+
+export default App;
